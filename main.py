0184ee92cde789a9cf3dadcd5069b2c715aa1700
vsgobbi <vitorsgobbi@hotmail.com>
2020-11-24 05:00:17 -0300

    added flask main file

---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from flask import Flask, jsonify, make_response, request
+from flask.json import JSONEncoder
+from models.client import PyMongoParser, authors_collection, news_collection
+from authors.authors import authors
+from news.news import news
+
+app = Flask(__name__)
+
+app.secret_key = "some big secret key to modify later..."
+app.config['JSON_AS_ASCII'] = False
+app.json_encoder = JSONEncoder
+app.register_blueprint(news)
+app.register_blueprint(authors)
+
+
+@app.route("/")
+@app.route("/api")
+def index():
+    return make_response(jsonify({"success": "our API is working!"}), 200)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return make_response(jsonify({"error": "route not found"}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
